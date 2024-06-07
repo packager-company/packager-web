8bd10a57ddc02c355863704d7a0d96daa0085d56
JoAraEsp <213345@ids.upchiapas.edu.mx>
2024-06-07 12:23:05 -0600

    home

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App'; 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <div>Hola gente</div>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);

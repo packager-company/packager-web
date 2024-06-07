9a90b172b8f6ccb77ce2b28092c784fc4140f9ce
LuisAxelMarínGarcía <213467@ids.upchiapas.edu.mx>
2024-06-07 02:04:10 -0600

    membresia

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MyMembership from './Pages/MyMembership/MyMembership';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div>Hola gente</div>
-  </React.StrictMode>,
-)
+    <MyMembership />
+  </React.StrictMode>
+);

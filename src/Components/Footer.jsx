ba94f11ca4686f4f279e03f133057cd7cc0f8f62
JaviXDLOL16 <100722991+JaviXDLOL16@users.noreply.github.com>
2024-06-06 19:15:01 -0600

    test

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/footer.module.css'
+
+function Footer() {
+  return (
+    <div className={styles.contFooter}>
+<p>sjssk</p>
+        </div>
+  )
+}
+
+export default Footer

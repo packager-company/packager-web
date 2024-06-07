932f9302e1696eb664b11035ab7556b21bd82264
Keyel <keyel@MacBook-Air-de-Keyel.local>
2024-06-07 03:35:04 -0600

    [ADD] Cotizacion

---
+++ src/Pages/Price/Price.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import style from './price.module.css'
+import Card from './Components/Card'
+import Button from './Components/Button'
+
+//className={style.}
+
+function Price() {
+    return (
+        <div className={style.containerPrice}>
+            <div className={style.divcontainer}>
+                <div className={style.divdatacontainerprice}  >
+                    <div className={style.divtitleprice}>
+                        <h1 className={style.titleprice}>Tarifas y tiempos</h1>
+                    </div>
+
+                    <form action=''>
+                        <div className={style.sectiondata} >
+                            <div>
+                                <label className={style.txtform} htmlFor="">Dirección de Origen</label>
+                                <input className={style.inputform} type="text" />
+                            </div>
+
+                            <div >
+                                <label className={style.txtform} htmlFor="">Dirección de Destino</label>
+                                <input className={style.inputform} type="text" />
+                            </div>
+                        </div>
+
+                        <div className={style.divdataprice}>
+                                <label className={style.txtform} htmlFor="">Peso del Paquete (kg)</label>
+                                <input className={style.inputform2} type="text" />
+
+                        </div>
+
+                        <div className={style.divbtn}>
+                            <Button text='Cotizar' />
+                        </div>
+                    </form>
+
+                    <Card 
+                    data1 = '3'
+                    data2 = '30'
+                    data3 = '1203'
+                    data4 = '120'
+                    data5 = '30'
+                    data6 = ''
+                    />
+
+                    <div className={style.divbtn}>
+                        <Button text='Recalcular' />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Price
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import CreateShipment from './Pages/CreateShipment/CreateShipment'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CreateShipment/>
+    <div>Hola gente</div>
   </React.StrictMode>,
 )

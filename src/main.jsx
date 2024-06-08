f0ea2afa010d43764c54942bec47dd4355056744
pizza <DonChicoPizza@hotmail.com>
2024-06-07 22:14:04 -0600

    Login admin y Login user

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import LoginAdmin from './Pages/Login-admin/Login-admin'
+import LoginUser from './Pages/Login-user/login-user'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div>Hola gente</div>
+    <LoginAdmin />
+    <LoginUser />
   </React.StrictMode>,
 )

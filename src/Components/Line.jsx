0881964f2cfdb4e3a3e95faa501e71e0e139cda7
JaviXDLOL16 <100722991+JaviXDLOL16@users.noreply.github.com>
2024-06-11 00:06:59 -0600

    add line

---
+++ src/Components/Line.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../styles/Line.module.css';
+
+
+function Line() {
+  return (
+    <div className={styles.contLine}>
+        <p>Aqui ira la lienea del diseño</p>
+    </div>
+  )
+}
+
+export default Line
--- src/Pages/_Layout/Root.jsx
+++ src/Pages/_Layout/Root.jsx
@@ -2,12 +2,14 @@ import { Link, Outlet } from 'react-router-dom'
 import styles from './styles.module.css'
 import Header from '../../Components/Header'
 import Footer from '../../Components/Footer'
+import Line from '../../Components/Line'
 
 
 export default function Root() {
     return (
         <div >
             <Header/>
+            <Line/>
             <div id={styles.content}><Outlet /></div>
             <Footer/>
         </div>
